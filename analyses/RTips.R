7f6f9b1cb17ed454ce2141aef5ab1d988ea8baa8
Alice Linder <alicelinder@college.harvard.edu>
2016-12-23 18:25:48 -0700

    Fig. 1 MDS Code added

---
+++ analyses/RTips.R
@@ -0,0 +1,5 @@
+# use stringsAsFactors and set to FALSE to prevent character columns from being converted to factor columnsre
+
+## use facet_wrap in ggplot2 to separate out all species
+
+## to search for something, use apropos()
