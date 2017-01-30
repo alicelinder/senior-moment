28c819e0833cf473ec3f3b0533682cbda02f4be0
Alice Linder <alicelinder@college.harvard.edu>
2017-01-30 16:16:06 -0500

    Convex Hull Code updated

--- analyses/output/Fig3_Convex-Hull.R
+++ analyses/output/Fig3_Convex-Hull.R
@@ -19,12 +19,41 @@ tree.traits["leaf.mass"] <- tree.traits$Fresh.mass - tree.traits$Dry.mass
 tree.traits <- tree.traits[,-13:-17]
 tree.traits <- tree.traits[,-14:-21]
 
-# clean up data and subset it
+# clean up data and subset it based on species
 traits <- select(tree.traits, Site, Species, Leaf.area, Stem.volume, 
                  Height, DBH, X.N, X.C, Stomatal.Length, Stomatal.Density, 
                  leaf.mass)
 
+class(traits)
+traits <- na.omit(traits)
+
+# find max and min values of each trait within each species
+range.function <- function(x) {
+  com.names <- unique(as.data.frame(traits$Species))
+  
+  apply(traits[com.names, ], MARGIN = 2, max) -
+  apply(traits[com.names, ], MARGIN = 2, min)
+}
+
+apply()
+
+
+?apply
+acepen <- max(traits[traits$Species == i, 2])
+
 # create matrix containing abundances of the species or presence/absence with 
 ## rows = sites and species = columns
+convhulln(traits)
 
 #convhulln(tree.traits$)
+
+range.function <- function(x) {
+  ## Get names of the species present in our community
+  com.names <- unique(as.data.frame(traits$Species))
+  
+  ## Calculate the range for each trait
+  apply(traits[com.names, ], MARGIN = 2, max)
+  
+}
+
+max(traits[com.names, ])
