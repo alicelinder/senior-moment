db4aedc14c809bacae44201e1ccc20c20a16ea28
Alice Linder <alicelinder@college.harvard.edu>
2017-02-07 17:55:08 -0500

    Linear models

--- analyses/input/Fig2-BA_vs_Lat.R
+++ analyses/input/Fig2-BA_vs_Lat.R
@@ -24,121 +24,36 @@ library(reshape)
 # load all data from source code
 source("Fig2-source.R")
 
+# ignore extra large DBH for FAGGUS value
+focal.centroid <- focal.centroid[-which(focal.centroid$sp == "FAGGRA" & focal.centroid$sum.BA > 20000),] 
 
-# plot intraspecific competition
-ggplot(focal,
-       aes(Lat, intra.comp.BA, color = sp)) +
+# ignore QUEALB for graphing purposes
+focal.centroid <- focal.centroid[-which(focal.centroid$sp == "QUEALB"),]
+
+ggplot(focal.centroid,
+       aes(minLatdiff, relative.BA, color = sp)) +
   geom_point() + 
   geom_smooth(method="lm", se=F) +
-  facet_wrap(~sp, ncol = 4)
+  facet_wrap(~sp, ncol = 3, scales = "free") +
+  xlab("Distance from Min. Latitude") +
+  ylab("Relative Basal Area")
+
 
 # model intraspecific competition
-summary(lm1 <- lm(intra.comp.BA ~ Lat, data = focal[focal$sp == "ACEPEN",]))
-summary(lm2 <- lm(intra.comp.BA ~ Lat, data = focal[focal$sp == "BETPAP",]))
-lme1 <- lmer(intra.comp.BA ~ Lat + (Lat | sp), data = focal)
+#summary(lm1 <- lm(relative.BA ~ minLatdiff, data = focal.centroid[focal.centroid$sp == "ACEPEN",]))
+#summary(lm1 <- lm(relative.BA ~ minLatdiff, data = focal.centroid[focal.centroid$sp == "BETPAP",]))
+#summary(lm1 <- lm(relative.BA ~ minLatdiff, data = focal.centroid[focal.centroid$sp == "CORALT",]))
+#summary(lm1 <- lm(relative.BA ~ minLatdiff, data = focal.centroid[focal.centroid$sp == "FAGGRA",]))
+#summary(lm1 <- lm(relative.BA ~ minLatdiff, data = focal.centroid[focal.centroid$sp == "HAMVIR",]))
+#summary(lm1 <- lm(relative.BA ~ minLatdiff, data = focal.centroid[focal.centroid$sp == "SORAME",]))
 
-# graph based on latitude
-ggplot(focal,
-       aes(Lat, competing.BA, color = sp)) +
-  geom_point() + 
-  geom_smooth(method="lm", se=F) +
-  facet_wrap(~sp, ncol = 4)
+#lme1 <- lmer(intra.comp.BA ~ Lat + (Lat | sp), data = focal)
 
-ggplot(focal,
-       aes(Lat, log.cBA, color = sp)) +
-  geom_point() + 
-  geom_smooth(method="lm", se=F) +
-  facet_wrap(~sp, ncol = 4)
+# model competition
+#summary(lm1 <- lm(relative.BA ~ Lat, data = focal[focal$sp == "ACEPEN",]))
 
-# Why excluding this individual Too large?
-ggplot(focal[-42,], 
-       aes(Site, Height, color = sp)) + 
-  geom_smooth( se = F, aes(color = sp)) +
-  geom_point()  + xlab("Site") + 
-  scale_x_continuous(labels = 
-                       c("HF","WM","GR","SH")) +
-  facet_wrap(~sp, ncol = 4) +
-  ylab("Height (m)") +
-  ggtitle("Shifts in Performance")
+#fixef(lme1)
+#ranef(lme1)
+#summary(lme1)
 
-# grah shows numbers (possibly due to infinite log values?)
-# not sure what you are expecting, this looks correct now
-
-ggplot(focal.small, 
-       aes(Site, log.cBA, color = sp)) + 
-  #geom_smooth( se = F, aes(color = sp)) +
-  geom_point()  + xlab("Site") + 
-  facet_wrap(~sp, ncol = 2) +
-  ylab("Fraction of total Basal Area")
-
-# no points appear on this graph
-# Site was not a factor previously, so as.numeric just makes a vector of NA -- can turn into a factor and then numeric, but not sure why you want this!
-ggplot(focal,
-       aes(as.numeric(as.factor(Site)), as.numeric(competing.BA), color = sp)) +
-  geom_point() + xlab("Site") +
-  facet_wrap(~sp, ncol=4)
-
-# Better, just use the factor Site
-ggplot(focal,
-       aes(Site, log.cBA, color = sp)) +
-         geom_point() + xlab("Site") +
-         facet_wrap(~sp, ncol=4)
-
-ggplot(focal.small, 
-       aes(Site, log.cBA, color = sp)) + 
-  #geom_smooth( se = F, aes(color = sp)) +
-  geom_point()  + xlab("Site") + 
-  #scale_x_continuous(labels = 
-                       #c("HF","WM","GR","SH")) +
-  facet_wrap(~sp, ncol = 2) +
-  ylab("Fraction of total Basal Area") 
-#ggtitle("Dominance of Small Woody Species")
-
-ggplot(focal.large, 
-       aes(Site, BA.Percentage, color = sp)) +  
-  #geom_smooth( se = F, aes(color = sp)) +
-  geom_point()  + xlab("Site") + 
-  scale_x_continuous(labels = 
-                       c("HF","WM","GR","SH")) +
-  facet_wrap(~sp, ncol = 2) +
-  ylab("Fraction of total Basal Area") 
-#ggtitle("Dominance of Large Woody Species")
-
-# relationship between height and BA.percentage
-## do it for 4 different sites for each species
-
-ggplot(focal,
-       aes(Site, Height, color = sp)) +
-  geom_point() + 
-  # geom_smooth(method="lm", se=F) +
-  facet_wrap(~sp, ncol = 4)
-which(focal$Height>100)
-ggplot(focal[-42,], 
-       aes(as.numeric(Site), Height, color = sp)) + 
-  geom_smooth( se = F, aes(color = sp)) +
-  geom_point()  + xlab("Site") + 
-  scale_x_continuous(labels = 
-                       c("HF","WM","GR","SH")) +
-  facet_wrap(~sp, ncol = 4) +
-  ylab("Height (m)") +
-  ggtitle("Shifts in Performance")
-
-# Analysis. Single linear models, very simple analysis here.
-
-summary(lm1 <- lm(log.cBA ~ Lat, data = focal[focal$sp == "ACEPEN",]))
-summary(lm2 <- lm(log.cBA ~ Lat, data = focal[focal$sp == "BETPAP",]))
-summary(lm3 <- lm(log.cBA ~ Lat, data = focal[focal$sp == "CORALT",]))
-summary(lm4 <- lm(log.cBA ~ Lat, data = focal[focal$sp == "FAGGRA",]))
-summary(lm5 <- lm(log.cBA ~ Lat, data = focal[focal$sp == "HAMVIR",]))
-summary(lm6 <- lm(log.cBA ~ Lat, data = focal[focal$sp == "QUEALB",]))
-summary(lm7 <- lm(log.cBA ~ Lat, data = focal[focal$sp == "SORAME",]))
-
-
-# Mixed effect model to use all species in single analysis
-lme1 <- lmer(competing.BA ~ Lat + (Lat | sp), data = focal)
-
-fixef(lme1)
-ranef(lme1)
-summary(lme1)
-
-ranef <- ranef(lme1)
+#ranef <- ranef(lme1)
---
+++ data/Fig1-MDS.R
@@ -0,0 +1,150 @@
+# MDS Plot of Species Composition
+## written by Alice Linder and Dan Flynn
+### updated by Alice on 22 Dec. 2016
+
+library(vegan)
+library(dplyr)
+library(tidyr)
+library(reshape)
+library(plyr)
+library(reshape2)
+library(ggplot2)
+
+rm(list = ls())
+#setwd("~/Library/Mobile Documents/com~apple~CloudDocs/GitHub/senior-moment/data")
+
+# setwd("~/Documents/git/senior-moment/data") # For Dan
+
+# MDS overstory
+d <- read.csv("all.species.dbh.csv", row.names = NULL)
+d <- d[,1:3]
+#d <- d2[,-2]
+
+# put data into correct format
+overstory <- distinct(d)
+head(overstory)
+#overstory <- rename(overstory, c("Comp.Species" = "Species")) 
+
+# check
+names(overstory)
+
+# SOMETHING WRONG HERE
+d <- melt(overstory, id = "Individual", measure.vars = "Comp.Species" )
+
+over.all <- as.data.frame(acast(d, Individual ~ value, length))
+
+head(over.all)
+over.all <- t(over.all)
+head(over.all)
+
+# Analysis and summarizing richness of the overstory
+richness <- apply(over.all, 2, sum)
+
+invisible(mds1 <- metaMDS(t(over.all), try = 100)) # use t() to change it so that the communities are rows, and species are columns, which is the format that vegan uses
+
+#plot(mds1) # ok, lots of scatter, good ordination
+
+overcomp <- data.frame(mds1$points)
+
+overcomp$s <- richness # add our species richness calculations to this data frame
+
+overcomp$sp <- substr(rownames(overcomp), 1, 6)  
+
+# Get the site by getting the last two characters of the overcomp rownames
+overcomp$site <- unlist(
+  lapply(strsplit(rownames(overcomp), "_"),
+         function(x) x[[2]]))
+
+# For each species, plot the species richness by site. Order sites by south -> north
+overcomp$site <- as.factor(overcomp$site)
+levels(overcomp$site) <- c(3, 1, 4, 2)
+overcomp$site <- factor(as.numeric(as.character(overcomp$site)), labels = c("HF", "WM", "GR", "SH"))
+
+# Clear differences with site, changing space along MDS1
+colz = alpha(c("#E7298A", "#1B9E77", "#D95F02", "#7570B3"), 0.5)
+
+# plot MDS overstory
+plot(mds1, type = "n",
+     xlim = c(-2, 2),
+     ylim = c(-1.2, 2))
+
+count = 1
+for(i in unique(overcomp$site)){
+  ordihull(mds1, group = overcomp$site, label = F,
+           draw = "polygon", col = colz[count],
+           show.groups = i)
+  count = count + 1
+}
+
+legend("topleft",
+       fill = colz,
+       legend = c("Harvard Forest", "White Mountains", "Grant", "St. Hippolyte"),
+       bty = "n")
+title("Overstory")
+
+rm(list = ls())
+
+# plot MDS understory
+
+d2 <- read.csv("understory.csv")
+
+#head(d2)
+
+# Data cleaning
+rownames(d2) = d2[,1] # move species names into rows
+d2 <- d2[,-1]
+
+head(d2)
+
+# Analysis
+# Summarizing the richness of the understory 
+#summary(d2)
+
+richness <- apply(d2, 2, sum)
+
+invisible(mds2 <- metaMDS(t(d2), try = 100)) # use t() to change it so that the communities are rows, and species are columns, which is the format that vegan uses
+
+#plot(mds2) # ok, lots of scatter, good ordination
+
+undercomp <- data.frame(mds2$points)
+
+undercomp$s <- richness # add our species richness calculations to this data frame
+
+undercomp$sp <- substr(rownames(undercomp), 1, 6)  
+
+# Get the site by getting the last two characters of the undercomp rownames
+undercomp$site <- unlist(
+  lapply(strsplit(rownames(undercomp), "_"),
+         function(x) x[[2]]))
+
+
+# For each species, plot the species richness by site. Order sites by south -> north
+undercomp$site <- as.factor(undercomp$site)
+levels(undercomp$site) <- c(3, 1, 4, 2)
+undercomp$site <- factor(as.numeric(as.character(undercomp$site)), labels = c("HF", "WM", "GR", "SH"))
+
+
+# Clear differences with site, changing space along MDS1
+colz = alpha(c("#E7298A", "#1B9E77", "#D95F02", "#7570B3"), 0.5)
+
+
+plot(mds2, type = "n",
+     xlim = c(-1.5, 1.5),
+     ylim = c(-1.2, 2)
+)
+
+count = 1
+for(i in unique(undercomp$site)){
+  ordihull(mds2, group = undercomp$site, label =F,
+           draw = "polygon", col = colz[count],
+           show.groups = i)
+  count = count + 1
+}
+
+legend("topleft",
+       fill = colz,
+       legend = c("Harvard Forest", "White Mountains", "Grant", "St. Hippolyte"),
+       bty = "n",
+       cex = 1.2)
+
+title("Understory")      
